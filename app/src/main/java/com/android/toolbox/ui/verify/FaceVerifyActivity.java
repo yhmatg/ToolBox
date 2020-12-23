7b62f4eaac139a9c9e94a10ec240358d1270e679
haomiao.yin <haomiao.yin@esimtek.com>
2020-12-23 10:34:36 +0800

    add take picture

--- app/src/main/java/com/android/toolbox/ui/verify/FaceVerifyActivity.java
+++ app/src/main/java/com/android/toolbox/ui/verify/FaceVerifyActivity.java
@@ -2,13 +2,19 @@ package com.android.toolbox.ui.verify;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.view.View;
 import android.widget.ImageView;
 
 import com.android.toolbox.R;
 import com.android.toolbox.base.activity.BaseActivity;
 import com.android.toolbox.base.presenter.AbstractPresenter;
+
+import java.io.File;
 
 import butterknife.BindView;
 import butterknife.OnClick;
@@ -17,6 +23,7 @@ public class FaceVerifyActivity extends BaseActivity {
     private static final int REQUEST_CODE_TAKE_PICTURE = 100;
     @BindView(R.id.im_face)
     ImageView facePic;
+    private File newFile;
 
     @Override
     public AbstractPresenter initPresenter() {
@@ -42,16 +49,15 @@ public class FaceVerifyActivity extends BaseActivity {
     public void performClick(View view) {
         switch (view.getId()) {
             case R.id.bt_camera:
-                 Intent openCameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE); // 系统常量， 启动相机的关键
-                startActivityForResult(openCameraIntent, REQUEST_CODE_TAKE_PICTURE); // 参数常量为自定义的request code, 在取返回结果时有用
-               /* // 打开相机Intent
+                //Intent openCameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE); // 系统常量， 启动相机的关键
+                //startActivityForResult(openCameraIntent, REQUEST_CODE_TAKE_PICTURE); // 参数常量为自定义的request code, 在取返回结果时有用
+                // 打开相机Intent
                 Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
                 // 给拍摄的照片指定存储位置
-                File imagePath = new File(getFilesDir(), "images"); // 指定文件
-                File newFile = new File(imagePath, "default_image.jpg");
+                newFile = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM), "default_image.jpg");
                 Uri fileUri = FileProvider.getUriForFile(this, "com.android.toolbox.provider", newFile); // 路径转换
                 cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri); //指定图片存放位置，指定后，在onActivityResult里得到的Data将为null
-                startActivityForResult(cameraIntent, REQUEST_CODE_TAKE_PICTURE);*/
+                startActivityForResult(cameraIntent, REQUEST_CODE_TAKE_PICTURE);
                 break;
         }
 
@@ -62,10 +68,17 @@ public class FaceVerifyActivity extends BaseActivity {
         switch (requestCode) {
             case REQUEST_CODE_TAKE_PICTURE:
                 // 此处写“如何获取图片”...
-                Bitmap bm = (Bitmap) data.getExtras().get("data");
-                facePic.setImageBitmap(bm);
+                if (data != null) {
+                    Bitmap bm = (Bitmap) data.getExtras().get("data");
+                    facePic.setImageBitmap(bm);
+                }
+                if(newFile != null && newFile.exists()){
+                    Bitmap bitmap = BitmapFactory.decodeFile(newFile.getAbsolutePath());
+                    facePic.setImageBitmap(bitmap);
+                    //删除文件，防止上传出错
+                    newFile.delete();
+                }
                 break;
-
         }
     }
 
