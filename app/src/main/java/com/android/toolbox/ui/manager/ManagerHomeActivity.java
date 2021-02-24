2c0f56246535dd2d3db19bbeb17bf880c75ba20a
haomiao.yin <haomiao.yin@esimtek.com>
2021-02-24 13:30:14 +0800

    modify login out dialog

--- app/src/main/java/com/android/toolbox/ui/manager/ManagerHomeActivity.java
+++ app/src/main/java/com/android/toolbox/ui/manager/ManagerHomeActivity.java
@@ -1,16 +1,25 @@
 package com.android.toolbox.ui.manager;
 
 import android.content.Intent;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.TextView;
 
+import com.afollestad.materialdialogs.MaterialDialog;
+import com.android.toolbox.HomeActivity;
 import com.android.toolbox.R;
 import com.android.toolbox.base.activity.BaseActivity;
 import com.android.toolbox.base.presenter.AbstractPresenter;
+import com.android.toolbox.ui.verify.BorrowBackToolActivity;
+import com.android.toolbox.utils.ScreenSizeUtils;
 
 import butterknife.OnClick;
 
 public class ManagerHomeActivity extends BaseActivity {
 
+    private MaterialDialog loginOutDialog;
     @Override
     public AbstractPresenter initPresenter() {
         return null;
@@ -50,9 +59,44 @@ public class ManagerHomeActivity extends BaseActivity {
                 startActivity(intent);
                 break;
             case R.id.ll_login_out:
-                finish();
+                showLoginOutDialog();
                 break;
+        }
+    }
 
+    public void showLoginOutDialog() {
+        if (loginOutDialog != null && !loginOutDialog.isShowing()) {
+            loginOutDialog.show();
+        } else {
+            View contentView = LayoutInflater.from(this).inflate(R.layout.login_out_dialog, null);
+            TextView cancel = contentView.findViewById(R.id.tv_no);
+            TextView sure = contentView.findViewById(R.id.tv_yes);
+            cancel.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    loginOutDialog.dismiss();
+                }
+            });
+            sure.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    loginOutDialog.dismiss();
+                    finish();
+                }
+            });
+            MaterialDialog.Builder builder = new MaterialDialog.Builder(this)
+                    .customView(contentView, false);
+            loginOutDialog = builder
+                    .canceledOnTouchOutside(false)
+                    .cancelable(false)
+                    .show();
+            Window window = loginOutDialog.getWindow();
+            window.setBackgroundDrawableResource(android.R.color.transparent);
+            // 设置宽度
+            WindowManager.LayoutParams layoutParams = window.getAttributes();
+            layoutParams.width = (int) (ScreenSizeUtils.getInstance(getApplication()).getScreenWidth() * 0.75f);
+            layoutParams.height = WindowManager.LayoutParams.WRAP_CONTENT;
+            window.setAttributes(layoutParams);
         }
     }
 }
